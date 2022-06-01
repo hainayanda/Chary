5501c41ad315f96e1b72e1cab79b675bdd58734f
Nayanda Haberty <hainayanda@outlook.com>
2022-06-01 14:18:33 +0700

    Add Atomic unit test

---
+++ Example/Tests/AtomicSpec.swift
@@ -0,0 +1,30 @@
+//
+//  AtomicSpec.swift
+//  Chary_Tests
+//
+//  Created by Nayanda Haberty on 01/06/22.
+//  Copyright © 2022 CocoaPods. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+import Chary
+
+class AtomicSpec: QuickSpec {
+    
+    override func spec() {
+        var atomicText: Atomic<String>!
+        beforeEach {
+            atomicText = Atomic(wrappedValue: "atomicText")
+        }
+        it("should assign the value atomically") {
+            atomicText.wrappedValue = "main"
+            DispatchQueue.global(qos: .utility).async {
+                atomicText.wrappedValue = "background"
+            }
+            expect(atomicText.wrappedValue).toEventually(equal("main"))
+            expect(atomicText.wrappedValue).toEventually(equal("background"))
+        }
+    }
+}
