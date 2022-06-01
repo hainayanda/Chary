faa23695b0c0ecf395b5b8e42af9fbd1fb44b560
Nayanda Haberty <hainayanda@outlook.com>
2022-06-01 15:13:23 +0700

    Update test_lib.sh

--- .github/scripts/test_lib.sh
+++ .github/scripts/test_lib.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -workspace Example/Chary.xcworkspace \
-            -scheme Pharos-Example \
+            -scheme Chary-Example \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
             clean test | xcpretty
